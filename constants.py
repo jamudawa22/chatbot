1d0efbcc083fbd0c652ffcd7a1bca6a8306621d8
Dawa Sherpa <56500281+dawasherpa-ui@users.noreply.github.com>
2023-10-14 21:28:55 +0545

    api constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "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"
